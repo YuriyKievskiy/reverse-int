89a7ed294a272e5995f48c54dc308036c3cfa9c9
YuriyKievskiy <kievskiy.y.a@gmail.com>
2022-01-06 20:10:52 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let sum = String(n);
+  sum = sum.replace(/[^0-9]/g, '');
+  arrNum = sum.split('');
+  arrReversed = arrNum.reverse();
+  let rev = arrReversed.join('');
+  rev = Number(rev);
+  return rev;
 }
